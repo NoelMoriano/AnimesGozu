3378ad980ef35d94219d2131c895f6324a10ba40
noelmoriano <noel.moriano@webuild.pe>
2022-07-28 18:09:07 -0500

    add new image

--- src/images/index.js
+++ src/images/index.js
@@ -3,3 +3,4 @@ export { default as ImgAvatar } from "./webp/avatar.webp";
 export { default as ImgAnime } from "./webp/anime-image.webp";
 export { default as ImgLogo } from "./others/logo-temp.png";
 export { default as NarutoBanner } from "./others/naruto-banner.jpg";
+export { default as Imalogo } from "./others/imalogo.png";
