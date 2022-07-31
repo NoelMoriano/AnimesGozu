d89c539eafb40b3db3f2fcbb0e5dcfd50db5a52f
angelgt <angel.gamonal@agenciaservitec.com>
2022-07-31 16:53:33 -0500

    add img background

--- src/images/index.js
+++ src/images/index.js
@@ -4,3 +4,5 @@ export { default as ImgAnime } from "./webp/anime-image.webp";
 export { default as ImgLogo } from "./others/logo-temp.png";
 export { default as NarutoBanner } from "./others/naruto-banner.jpg";
 export { default as Imalogo } from "./others/imalogo.png";
+export { default as ImgBackground } from "./webp/bg6.webp";
+export { default as ImgCardOnePiece } from "./home/animes/aventura/image 13.png";
