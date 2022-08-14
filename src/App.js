7531e82e62407059822ec36a4444573cb019a19e
noelMoriano <nmoriano26@gmail.com>
2022-08-14 18:12:00 -0500

    update app

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { Router } from "./router";
 import { BrowserRouter } from "react-router-dom";
 import { GlobalStyles } from "./styles/themes/GlobalStyle";
@@ -6,40 +6,21 @@ import { ThemeProvider } from "styled-components";
 import { yup } from "./config";
 import { setLocale } from "yup";
 import { theme } from "./styles";
-import { animeServerApi } from "./firebase";
-import { Spinner } from "./components";
+import { AnimesProvider, useAnimes } from "./providers/Animes";
 
 export const App = () => {
-  const [animes, setAnimes] = useState([]);
-  const [loading, setLoading] = useState(true);
-
   useEffect(() => {
-    animesFetch();
     setLocale(yup["es"]);
   }, []);
 
-  const animesFetch = async () => {
-    try {
-      const response = await fetch(`${animeServerApi}/animes`);
-      const result = await response.json();
-      setAnimes(result);
-
-      console.log("data->", result);
-    } catch (error) {
-      console.log("error->", error);
-    } finally {
-      setLoading(false);
-    }
-  };
-
-  if (loading) return <Spinner height="90vh" />;
-
   return (
     <BrowserRouter>
-      <ThemeProvider theme={theme}>
-        <GlobalStyles />
-        <Router animes={animes} />
-      </ThemeProvider>
+      <AnimesProvider>
+        <ThemeProvider theme={theme}>
+          <GlobalStyles />
+          <Router />
+        </ThemeProvider>
+      </AnimesProvider>
     </BrowserRouter>
   );
 };
