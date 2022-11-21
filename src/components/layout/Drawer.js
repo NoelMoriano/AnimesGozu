a47cce10d7e3db4a0e881d710aee5f7cbe46ed18
noeLMoriano <noel.moriano@webuild.pe>
2022-11-21 00:51:50 -0500

    update menu content

--- src/components/layout/Drawer.js
+++ src/components/layout/Drawer.js
@@ -27,18 +27,28 @@ const ComponentLogout = ({ authUser, logout, onNavigateTo }) => (
     {authUser ? (
       <UserMenu onLogout={logout} />
     ) : (
-      <div className="wrapper-buttons">
-        <br />
-        <Button size="medium" onClick={() => onNavigateTo("/login")}>
-          Iniciar sesion
-        </Button>
-        <Button
-          size="medium"
-          type="tertiary"
-          onClick={() => onNavigateTo("/register")}
-        >
-          Registrarse
-        </Button>
+      <div className="wrapper-no-auth-user">
+        <div className="wrapper-buttons">
+          <br />
+          <Button size="medium" onClick={() => onNavigateTo("/login")}>
+            Iniciar sesion
+          </Button>
+          <Button
+            size="medium"
+            type="tertiary"
+            onClick={() => onNavigateTo("/register")}
+          >
+            Registrarse
+          </Button>
+        </div>
+        <div className="wrapper-messaje">
+          <p>
+            Al registrarte podrás solicitar tus animes favoritos y nosotros te
+            lo enlistamos. Para nosotros es super importante su comodidad al
+            usar nuestra plataforma, te agradeceremos mucho tus sugerencias para
+            mejorar la plataforma ^^
+          </p>
+        </div>
       </div>
     )}
   </>
@@ -48,10 +58,17 @@ const Container = styled.div`
   height: auto;
   position: sticky;
   top: 90px;
-  .wrapper-buttons {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
+  .wrapper-no-auth-user {
+    padding: 0 1em;
+    .wrapper-buttons {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+    }
+    .wrapper-messaje {
+      text-align: center;
+      margin-top: 1em;
+    }
   }
 `;
